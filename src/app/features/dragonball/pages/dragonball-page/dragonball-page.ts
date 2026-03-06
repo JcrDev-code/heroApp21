ce12a648d49dc39ffa44a3b893c6f94ce177f812
josdev <chaqquererj@gmail.com>
2026-03-06 11:31:02 -0500

    feat: implemetar control flow

--- src/app/features/dragonball/pages/dragonball-page/dragonball-page.ts
+++ src/app/features/dragonball/pages/dragonball-page/dragonball-page.ts
@@ -1,5 +1,10 @@
 import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -10,11 +15,26 @@ export class DragonballPage {
   name = signal('Goku');
   power = signal(9000);
 
+  characters = signal<Character[]>([]);
+
   heroInfo = computed(() => {
     return `${this.name()} tiene un poder de ${this.power()}`;
   });
 
-  addHero() {
-    console.log(this.heroInfo());
+  addCharacter() {
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((chars) => [...chars, newCharacter]);
+    this.resetFields();
+  }
+
+  isDisable = computed(() => !this.name() || this.power() <= 0);
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
   }
 }
